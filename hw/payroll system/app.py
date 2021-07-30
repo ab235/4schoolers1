a7cd25989bae885166ebb4648f3aa575e3149fe0
Aditya <aditya.bachina@gmail.com>
2021-07-30 12:54:15 -0400

    some errors, but login/register/add user works

--- hw/payroll system/app.py
+++ hw/payroll system/app.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'dfghjiouhgdashswevnohshshshidnasiodn'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///payroll.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///payrolltwo.db'
 db = SQLAlchemy(app)
 
 class Company(db.Model):
     db.__tablename__ = 'company'
-    id = db.Column(db.Integer, unique=True, nullable=False, primary_key = True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, nullable=False, primary_key = True)
+    name = db.Column(db.String(80), nullable=False)
     balance = db.Column(db.Float, nullable=False)
+    admin_id = db.Column(db.String(200))
     employees = db.relationship("User", back_populates="company", enable_typechecks=False)
 
     def __repr__(self):
@@ -27,7 +28,7 @@ class Company(db.Model):
 
 class User(db.Model):
     db.__tablename__ = 'user'
-    id = db.Column(db.Integer, unique=True, nullable=False, primary_key = True)
+    id = db.Column(db.Integer, nullable=False, primary_key = True)
     name = db.Column(db.String(80), nullable=False)
     ssn = db.Column(db.Integer, nullable=False)
     uname = db.Column(db.String(80), nullable=False)
@@ -41,9 +42,14 @@ class User(db.Model):
     company = db.relationship("Company", back_populates="employees")
 
     def __repr__(self):
-        return self.name
+        return str(self.uname)
 
     def is_admin(self):
+        id_list = self.company.admin_id
+        id_list = id_list.split(",")
+        id_list = [int(x) for x in id_list]
+        if (self.id in id_list):
+            return True
         return False
 
     @staticmethod
@@ -51,8 +57,8 @@ class User(db.Model):
         db.session.add(user)
         db.session.commit()
     @staticmethod
-    def update(user, db):
-        puser = User.query.filter_by(id=user.id).first()
+    def update(user, id, db):
+        puser = User.query.filter_by(id=id).first()
         puser.name = user.name
         puser.ssn = user.ssn
         puser.salary = user.salary
@@ -62,13 +68,19 @@ class User(db.Model):
         puser.balance = user.balance
         puser.uname = user.uname
         puser.password = user.password
+        print(user.company.employees)
+        del user
         db.session.commit()
 
     @staticmethod
     def get_user(uname, db):
         return User.query.filter_by(uname=uname).first()
-
-class Admin(User):
+    @staticmethod
+    def get_user_l(uname, lis):
+        for i in range(len(lis)):
+            if (lis[i].uname == uname):
+                return lis[i]
+        return False
     def pay_user(id):
         user = User.query.filter_by(id=id).first()
         if (user.company.balance - user.salary < 0):
@@ -76,23 +88,6 @@ class Admin(User):
         user.balance += user.salary
         user.company.balance -= user.salary
         db.session.commit()
-        return True
-    @staticmethod
-    def register_user(self, name, ssn, uname, password, salary, attendance, rating, level, balance):
-        user = User()
-        user.id = (len(User.query.filter_by(company = self.company)))
-        user.name = name
-        user.ssn = ssn
-        user.uname = uname
-        user.password = password
-        user.salary = salary
-        user.attendance = attendance
-        user.rating = rating
-        user.level = level
-        user.balance = balance
-        user.company = self.company
-        User.save_db(user, db)
-    def is_admin(self):
         return True
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -112,18 +107,21 @@ def register():
             company.id = len(Company.query.all())
             company.name = name
             company.balance = money
-            company.employees.append(Admin())
-            company.employees[0].id = len(User.query.all())
-            company.employees[0].name = "_"
-            company.employees[0].ssn = -1
-            company.employees[0].uname = "root"
-            company.employees[0].password = "1234"
-            company.employees[0].salary = 0.0
-            company.employees[0].rating = 0.0
-            company.employees[0].attendance = 0.0
-            company.employees[0].level = 1
-            company.employees[0].balance = 0.0
-            company.employees[0].company = company
+            company.admin_id = str(len(User.query.all()))
+            Company.save_db(company, db)
+            first = User()
+            first.id = len(User.query.all())
+            first.name = "()"
+            first.ssn = -1
+            first.uname = "root"
+            first.password = "1234"
+            first.salary = 0.0
+            first.rating = 0.0
+            first.attendance = 0.0
+            first.level = 1
+            first.balance = 0.0
+            first.company = company
+            company.employees.append(first)
 
             session['user'] = "root"
             Company.save_db(company, db)
@@ -138,12 +136,12 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         company_name = request.form.get('company')
-        company = db.get_company(company_name, db)
+        company = Company.get_company(company_name, db)
         if (company):
-            if (company.employees.get_user(username, db)):
-                if (company.employees.get_user(username, db).password == password):
+            if (User.get_user_l(username, company.employees)):
+                if (User.get_user_l(username, company.employees).password == password):
                     session['user'] = username
-                    if (company.employees.get_user(username, db).is_admin()):
+                    if (User.get_user_l(session['user'], company.employees).is_admin()):
                         return redirect(url_for('dashboard', name = company.name))
                     else:
                         return redirect(url_for('profile', name = username, cname = company.name))
@@ -161,12 +159,10 @@ def dashboard(name):
         emps = Company.get_company(name, db).employees
         message = ''
         if (session['user']):
-            if (emps.get_user(session['user'], db)):
-                if (emps.get_user(session['user'], db).is_admin()):
+            if (User.get_user_l(session['user'], emps)):
+                if (User.get_user_l(session['user'], emps).is_admin()):
                     if (request.method == 'POST'):
-                        if (request.form.get('new_user')):
-                            return redirect(url_for('ruser', name=name))
-                        admin = emps.get_user(session['user'], db)
+                        admin = User.get_user_l(session['user'], emps)
                         ids = [x.id for x in emps]
                         for x in ids:
                             if (str(x) in request.form):
@@ -176,11 +172,11 @@ def dashboard(name):
     return redirect(url_for('index'))
 @app.route('/profile/<cname>/<name>', methods = ['GET', 'POST'])
 def profile(cname, name):
-    if (Company.get_company(cname), db):
+    if (Company.get_company(cname, db)):
         emps = Company.get_company(cname, db).employees
         if (session['user']):
-            if (emps.get_user(session['user'], db)):
-                user = emps.get_user(session['user'], db)
+            if (User.get_user_l(session['user'], emps)):
+                user = User.get_user_l(session['user'], emps)
                 return render_template('profile.jinja', user=user)
     return render_template('profile.jinja')
 @app.route('/ruser/<name>', methods = ['GET', 'POST'])
@@ -188,10 +184,50 @@ def ruser(name):
     if (Company.get_company(name, db)):
         emps = Company.get_company(name, db).employees
         message = ''
+        check = False
         if (session['user']):
-            if (emps.get_user(session['user'], db)):
-                if (emps.get_user(session['user'], db).is_admin()):
-                    return render_template("profile.jinja")
+            if (User.get_user_l(session['user'], emps)):
+                if (User.get_user_l(session['user'], emps).is_admin()):
+                    if (request.method == "POST"):
+                        id1 = request.form.get('id')
+                        name = request.form.get('name')
+                        ssn = request.form.get('ssn')
+                        uname = request.form.get('uname')
+                        if not ((uname in [x.uname for x in emps]) and (id1 not in [x.id for x in emps])):
+                            password = request.form.get('password')
+                            salary = request.form.get('salary')
+                            attendance = request.form.get('attendance')
+                            rating = request.form.get('rating')
+                            level = request.form.get('level')
+                            user = User()
+                            user.id = len(User.query.all())
+                            user.balance = 0.0
+                            user.name = name
+                            user.ssn = ssn
+                            user.uname = uname
+                            user.password = password
+                            user.salary = salary
+                            user.attendance = attendance
+                            user.rating = rating
+                            user.level = level
+                            user.company = User.get_user_l(session['user'], emps).company
+                            for i in range(len(emps)):
+                                if (emps[i].id == int(id1)):
+                                    user.balance = emps[i].balance
+                                    User.update(user, id1, db)
+                                    print(User.query.all())
+                                    check = True
+                                    break
+                            print(check)
+                            if (not check):
+                                print(check)
+                                User.save_db(user, db)
+                            return redirect(url_for('dashboard', name = emps[0].company.name))
+                        else:
+                            message = "Username taken."
+
+
+                    return render_template("ruser.jinja", message = message)
 
 
 
